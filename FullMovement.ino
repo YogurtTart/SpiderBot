1e26d6c2076a967e89244e2b9a0bd0ba6d5e0e7b
YogurtTart <137908838+YogurtTart@users.noreply.github.com>
2025-01-22 13:03:53 +0800

    Add files via upload

--- FullMovement.ino
+++ FullMovement.ino
@@ -25,7 +25,7 @@ void setup() {
 
   Serial.println("Servo service started");
   initial();
-  delay(2000);
+  delay(1000);
 }
 
 void loop() {
@@ -33,36 +33,36 @@ void loop() {
   //Standing Function
   Serial.println("Stand");
   stand();
-  delay(2000);
+  delay(1000);
 
   //walk function
   Serial.println("Walk");
   forward();
-  delay(2000);
+  delay(1000);
 
   //Sitting Function
   Serial.println("Sit");
   sit();
-  delay(2000);
+  delay(1000);
 
   //Wave Function
   Serial.println("Wave");
   wave();
-  delay(2000);
+  delay(1000);
 
   //Stand Function
   Serial.println("Stand");
   stand();
-  delay(2000);
+  delay(1000);
   
 }
 
 void initial(){
 
   Shoulder(135, 135, 45, 45);// 4, 13, 10, 7
-  delay(2000); // Pause for 2 seconds after shoulder movements
+  delay(1000); // Pause for 1 seconds after shoulder movements
   Elbows(140, 40, 40, 140);// 8 , 11 , 2 ,5 
-  delay(2000); // Pause for 2 seconds after elbow movements
+  delay(1000); // Pause for 1 seconds after elbow movements
   Hands(70, 70, 110, 110);// 3, 12, 6, 9
 
 }
@@ -70,7 +70,6 @@ void initial(){
 void sit(){
 
   Elbows(140, 40, 40, 140);// 8 , 11 , 2 ,5 
-  delay(1000); // Pause for 4 seconds after elbow movements
 
 /* Extra feature
   for(int i = 0; i <3; i++){
@@ -85,25 +84,24 @@ void sit(){
 void stand() {
 
  Elbows(100, 80, 80, 100); // 8 , 11 , 2 ,5  
- delay(1000); // Pause for 2 seconds after elbow movements
   
 }
 
 void wave() {
 
 Elbows(140, 40, 20, 140);// 8 , 11 , 2 ,5 
-delay(1000); // Pause for 2 seconds after elbow movements
+delay(500); // Pause for 0.5 seconds after elbow movements
 Hands(150, 70, 110, 110);// 3, 12, 6, 9 
 
 for(int i = 0; i <3; i++){
   Shoulder(155, 135, 45, 45);// 4, 13, 10, 7
-  delay(200); // Pause for 4 seconds after elbow movements
+  delay(200); // Pause for 0.2 seconds after elbow movements
   Shoulder(135, 135, 45, 45);// 4, 13, 10, 7
   }
 
-  delay(200); // Pause for 4 seconds after elbow movements
+  delay(200); // Pause for 0.2 seconds after elbow movements
   Hands(70, 70, 110, 110);// 3, 12, 6, 9
-  delay(200); // Pause for 4 seconds after elbow movements
+  delay(200); // Pause for 0.2 seconds after elbow movements
   Elbows(140, 40, 40, 140);// 8 , 11 , 2 ,5 
 
 }
@@ -111,53 +109,53 @@ for(int i = 0; i <3; i++){
 void forward(){
 
 Shoulder(120, 125, 60, 55);// 4, 13, 10, 7
-delay(500); 
+delay(200); 
 
 Elbows(110, 70, 70, 110); // 8 , 11 , 2 ,5 
 
 
-for(int i = 0; i <1; i++){
+for(int i = 0; i <4; i++){
 
 //top right leg
 Elbows(110, 70, 60, 110); // 8 , 11 , 2 ,5 
-delay(200);
+delay(100);
 Shoulder(160, 110, 60, 70);// 4, 13, 10, 7
-delay(200);
+delay(100);
 Elbows(110, 70, 70, 110); // 8 , 11 , 2 ,5 
-delay(500);
+delay(100);
 
 
 //btm left leg
 Elbows(110, 60, 70, 110); // 8 , 11 , 2 ,5   
-delay(200); 
+delay(100); 
 Shoulder(160, 100, 60, 70);// 4, 13, 10, 7
-delay(200);
+delay(100);
 Elbows(110, 70, 70, 110); // 8 , 11 , 2 ,5  
-delay(500);
+delay(100);
 
 
 //btm right leg
 
 Elbows(110, 70, 70, 120); // 8 , 11 , 2 ,5  
-delay(200); 
+delay(100); 
 Shoulder(160, 100, 60, 110);// 4, 13, 10, 7
-delay(200);
+delay(100);
 Elbows(110, 70, 70, 110); // 8 , 11 , 2 ,5  
-delay(500); 
+delay(100); 
 
 
 //top left leg
 Elbows(120, 70, 70, 110); // 8 , 11 , 2 ,5  
-delay(200);
+delay(100);
 Shoulder(160, 80, 20, 100);// 4, 13, 10, 7
-delay(200);
+delay(100);
 Elbows(110, 70, 70, 110); // 8 , 11 , 2 ,5 
-delay(500); 
+delay(100); 
 
 
 //Move all forward
 Shoulder(120, 125, 60, 55);// 4, 13, 10, 7
-delay(500);
+delay(100);
 
 
 
